eebd4d12a9d89675b8575d0bc330780886170133
Fransou <philippe.form@gmail.com>
2024-07-11 13:45:06 -0400

    [eval|test] Full evaluation functioned

--- molDistill/run_eval_while_training.py
+++ molDistill/run_eval_while_training.py
@@ -5,6 +5,7 @@ import time
 import argparse
 
 import pandas as pd
+import wandb
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -12,40 +13,109 @@ logger.setLevel(logging.INFO)
 # while true; launch the downstream_eval.py script if a new model is available in the folder specified
 # by the MODEL_PATH variable that has not been seen before
 
+ALL_DATASETS = [
+    "hERG",
+    "hERG_Karim",
+    "AMES",
+    "DILI",
+    "Carcinogens_Lagunin",
+    "Skin__Reaction",
+    "Tox21",
+    "ClinTox",
+    "PAMPA_NCATS",
+    "HIA_Hou",
+    "Pgp_Broccatelli",
+    "Bioavailability_Ma",
+    "BBB_Martins",
+    "CYP2C19_Veith",
+    "CYP2D6_Veith",
+    "CYP3A4_Veith",
+    "CYP1A2_Veith",
+    "CYP2C9_Veith",
+    "CYP2C9_Substrate_CarbonMangels",
+    "CYP2D6_Substrate_CarbonMangels",
+    "CYP3A4_Substrate_CarbonMangels",
+    "Caco2_Wang",
+    "Lipophilicity_AstraZeneca",
+    "Solubility_AqSolDB",
+    "HydrationFreeEnergy_FreeSolv",
+    "PPBR_AZ",
+    "VDss_Lombardo",
+    "Half_Life_Obach",
+    "Clearance_Hepatocyte_AZ",
+    "Clearance_Microsome_AZ",
+    "LD50_Zhu",
+    "HIV",
+]
+
+
+def launch_model_eval(model, MODEL_PATH):
+    logger.info(f"Launching eval for {model}")
+    if args.sbatch:
+        os.system(f"sbatch eval.sh custom:{os.path.join(MODEL_PATH, model)} 5")
+    else:
+        os.system(
+            f"python molDistill/downstream_eval.py --embedders custom:{os.path.join(MODEL_PATH, model)} --datasets hERG --test"
+        )
+
+
+def log_eval_results(model, MODEL_PATH):
+    logger.info(f"Logging results for {model}")
+    df = pd.read_csv(os.path.join(MODEL_PATH, model.replace(".pth", ".csv")))
+    epoch = int(model.replace(".pth", "").split("_")[-1])
+    df = df.drop(axis=1, columns=["Unnamed: 0", "embedder"])
+    df = df.groupby(["dataset"]).mean().reset_index()
+    all_logs = {
+        f"eval_perfs_{dataset}": df[df.dataset == dataset].metric_test.values[0]
+        for dataset in df.dataset.unique()
+    }
+    all_logs["eval_perfs"] = df.metric_test.mean()
+    all_logs["epoch"] = epoch
+    wandb.log(all_logs)
+
+
 if __name__ == "__main__":
+    wandb.init(
+        project="mol-distill-downs-ckpt",
+        allow_val_change=True,
+    )
+    for dataset in ALL_DATASETS:
+        wandb.define_metric(f"eval_perfs_{dataset}", step_metric="epoch")
+    wandb.define_metric("eval_perfs", step_metric="epoch")
+
     parser = argparse.ArgumentParser()
     parser.add_argument("MODEL_PATH", type=str)
     parser.add_argument("--sbatch", action="store_true")
     args = parser.parse_args()
     MODEL_PATH = args.MODEL_PATH
     checked_models = ["best_model.pth"]
+    logged_models = ["best_model.pth"]
     last_round = False
     continue_training = True
     while continue_training:
         # Get all the models in the folder
         models = os.listdir(MODEL_PATH)
+        models = [model for model in models if model.endswith(".pth")]
         # For every model
         for model in models:
             # If the model has not been checked
-            if model.endswith(".pth") and not model in checked_models:
-                # Launch the downstream eval script
-                logger.info(f"Launching eval for {model}")
-                if args.sbatch:
-                    os.system(
-                        f"sbatch eval.sh custom:{os.path.join(MODEL_PATH, model)} 5"
-                    )
-                else:
-                    os.system(
-                        f"python molDistill/downstream_eval.py --embedders custom:{os.path.join(MODEL_PATH, model)} --datasets hERG --test"
-                    )
-                # Add the model to the checked models
+            if not model in checked_models:
+                launch_model_eval(model, MODEL_PATH)
                 checked_models.append(model)
+            if (
+                os.path.exists(os.path.join(MODEL_PATH, model.replace(".pth", ".csv")))
+                and not model in logged_models
+            ):
+                log_eval_results(model, MODEL_PATH)
+                logged_models.append(model)
 
-            # if stop.txt is in the directory, stop the script
-            if model == "stop.txt":
-                if last_round:
-                    continue_training = False
-                last_round = True
+        if (
+            os.path.exists(os.path.join(MODEL_PATH, "stop.txt"))
+            and logged_models == checked_models
+        ):
+            if last_round:
+                continue_training = False
+            last_round = True
 
         logger.info("Sleeping for 10 seconds")
         time.sleep(10)
@@ -53,21 +123,6 @@ if __name__ == "__main__":
     checked_models = checked_models[1:]
 
     logger.info("Finished logging all results")
-
-    all_csv_exist = False
-    while not all_csv_exist:
-        all_csv_exist = all(
-            os.path.exists(os.path.join(MODEL_PATH, model.replace(".pth", ".csv")))
-            for model in checked_models
-        )
-        time.sleep(10)
-
-    import wandb
-
-    wandb.init(
-        project="mol-distill-downs-ckpt",
-        allow_val_change=True,
-    )
 
     df = pd.concat(
         [
@@ -79,23 +134,5 @@ if __name__ == "__main__":
     df = df.drop(axis=1, columns=["Unnamed: 0", "embedder"])
     df = df.groupby(["epoch", "dataset"]).mean().reset_index()
     wandb.log({"table": wandb.Table(dataframe=df)})
-    df_grouped = df.groupby("epoch")["metric_test"].mean().reset_index()
-    for ep in df_grouped.epoch:
-        wandb.log(
-            {
-                "eval_perfs": df_grouped[df_grouped.epoch == ep].metric_test.values[0],
-                "epoch": ep,
-            }
-        )
 
-    # log one lineplot for each dataset
-    for dataset in df.dataset.unique():
-        df_dataset = wandb.Table(dataframe=df[df.dataset == dataset])
-        wandb.log(
-            {
-                f"eval_perfs_{dataset}": wandb.plot.line(
-                    df_dataset, x="epoch", y="metric_test", title=dataset
-                )
-            }
-        )
     wandb.finish()
