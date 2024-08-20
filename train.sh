21f2510f8e0fdd83ea1c2fb31b85dd4dc7ac2eeb
Fransou <philippe.form@gmail.com>
2024-08-20 11:19:32 -0400

    divide by emb shape

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=distill_mol
 #SBATCH --account=def-ibenayed
-#SBATCH --time=0-30:00:00
+#SBATCH --time=0-48:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --mem=200G
 #SBATCH --cpus-per-task=8
