c019e0c41f75f3eee65318676b50df07c6f02426
Fransou <philippe.form@gmail.com>
2024-09-05 11:03:21 -0400

    Better sampler for eval

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=distill_mol
 #SBATCH --account=def-ibenayed
-#SBATCH --time=5-00:00:00
+#SBATCH --time=3-00:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --mem=200G
 #SBATCH --cpus-per-task=8
